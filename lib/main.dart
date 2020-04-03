33acacf609d4435f6660a807a01238a1bee4d0f2
Skalozub <norton47@yandex.by>
2020-04-04 00:57:08 +0300

    fifth lesson third part part https://www.youtube.com/watch?v=YjMcekfMLOA

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,55 @@
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text("Main menu")),
+      key: _scaffoldKey,
+      appBar: AppBar(title: Text("Less or more")),
       body: Center(
         child: Column(children: <Widget>[
-          RaisedButton(onPressed: (){
-            Navigator.pushNamed(context, '/second');
-          }, child: Text("Open second window")),
-          RaisedButton(onPressed: (){
-            Navigator.pushNamed(context, '/second/123');
-          }, child: Text("Open second window")),
+          RaisedButton(onPressed: () async {
+            bool value = await Navigator.push(context, PageRouteBuilder(
+              opaque: false, pageBuilder: (context, _, __) => MyPopup(),
+              transitionsBuilder: (___, animation, ____, child) {
+               return FadeTransition(
+                   opacity: animation,
+                 child: ScaleTransition(scale: animation, child: child),
+               );
+            })
+            );
+
+            if (value) _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text("More"), backgroundColor: Colors.green));
+            else _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text('Less'), backgroundColor: Colors.red,));
+          }, child: Text("Find counter")),
         ])
       ),
     );
   }
 }
 
-class SecondScreen extends StatelessWidget {
-  String _id;
-
-  SecondScreen({String id}): _id = id;
-
+class MyPopup extends StatelessWidget {
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: Text("Second screen $_id")),
-      body: Center(child: RaisedButton(onPressed: (){
-        Navigator.pop(context);
-      }, child: Text("BACK!"),)),
+    return AlertDialog(
+      title: Text('Your answer'),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: (){Navigator.pop(context, true);},
+          child: Text('more'),
+        ),
+        FlatButton(
+          onPressed: (){Navigator.pop(context, false);},
+          child: Text('less'),
+        )
+      ],
     );
   }
 }
 
 void main() => runApp(
 new MaterialApp(
-  debugShowCheckedModeBanner: false,
-  initialRoute: '/',
-  routes: {
-    '/':(BuildContext context) => MainScreen(),
-    '/second': (BuildContext context) => SecondScreen(),
-  },
-  // ignore: missing_return
-  onGenerateRoute: (routeSettings){
-    var path = routeSettings.name.split("/");
-
-    if (path[1] == "second") {
-      return new MaterialPageRoute(builder: (builder) => new SecondScreen(id: path[2]),
-      settings: routeSettings);
-    }
-  },
+  home: MainScreen(),
 )
 );
