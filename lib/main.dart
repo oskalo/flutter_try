6fed363f5c4a9bf3bea4e75d14c392aa51c0ab91
Skalozub <norton47@yandex.by>
2020-04-03 23:38:08 +0300

    fifth lesson first part https://www.youtube.com/watch?v=YjMcekfMLOA

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:validators/validators.dart';
 
-enum GenderList{male, female}
-
-class MyForm extends StatefulWidget {
+class MainScreen extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() => MyFormState();
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text("Main menu")),
+      body: Center(
+        child: RaisedButton(onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen()));
+        }, child: Text("Open second window")),
+      ),
+    );
+  }
 }
 
-class MyFormState extends State {
-
-  GenderList _gender;
-  final _fromKey = GlobalKey<FormState>();
-  bool _agreement = false;
-
+class SecondScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return new Container(
-        padding: EdgeInsets.all(10.0),
-        child: new Form(key: _fromKey, child: new Column(children: <Widget>[
-          // NAME
-          new Text("User name ", style: TextStyle(fontSize: 20.0)),
-          new TextFormField(validator: (value){
-            if (value.isEmpty) return "please enter your name";
-          }),
-
-          new SizedBox(height: 20.0),
-
-          // MAIL
-          new Text("email", style: TextStyle(fontSize: 20.0)),
-          new TextFormField(validator: (value){
-            if (value.isEmpty) return "please enter your email";
-            if (!isEmail(value)) return "it is not  E-mail";
-            return null;
-          }),
-
-          new SizedBox(height: 20.0),
-
-          // GENDER
-          new Text("gender", style: TextStyle(fontSize: 20.0)),
-          new RadioListTile(title: Text("male"), value: GenderList.male, groupValue: _gender, onChanged: (GenderList value){
-            setState(() {
-              _gender = value;
-            });
-          }),
-          new RadioListTile(title: Text("female"), value: GenderList.female, groupValue: _gender, onChanged: (GenderList value){
-            setState(() {
-              _gender = value;
-            });
-          }),
-
-          new SizedBox(height: 20.0),
-
-          new CheckboxListTile(value: _agreement,
-              title: new Text("I egree to sell all"),
-              onChanged: (bool value) => setState(() => _agreement = value)),
-          new SizedBox(height: 20.0),
-
-          new RaisedButton(onPressed: (){
-            if (_fromKey.currentState.validate()) {
-              Color color = Colors.red;
-              String text;
-
-
-              if (_gender == null) text = "You didn't chouse gender";
-              else if(_agreement == false) text = "pull agree mather fucker";
-              else {
-                text = "Form good";
-                color = Colors.green;
-              }
-
-              Scaffold.of(context).showSnackBar(new SnackBar(content: new Text(text), backgroundColor: color));
-            }
-
-
-          }, child: Text("Check"), color: Colors.blue, textColor: Colors.white)
-        ],))
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text("Second screen")),
+      body: Center(child: RaisedButton(onPressed: (){
+        Navigator.pop(context);
+      }, child: Text("BACK!"),)),
     );
   }
 }
@@ -85,7 +31,7 @@ new MaterialApp(
   debugShowCheckedModeBanner: false,
   home: new Scaffold(
     appBar: new AppBar(title: new Text("data")),
-    body: MyForm()
+    body: MainScreen()
   )
 )
 );
