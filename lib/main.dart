685b03e0a2b041ac7d88bc87d6760197685e80b7
Skalozub <norton47@yandex.by>
2020-04-04 10:44:20 +0300

    sixth lesson https://www.youtube.com/watch?v=aaL_SP-nuOE Flutter Урок 6. Асинхронные методы async и await

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,60 @@
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatelessWidget {
-  final _scaffoldKey = GlobalKey<ScaffoldState>();
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(title: Text("Less or more")),
-      body: Center(
-        child: Column(children: <Widget>[
-          RaisedButton(onPressed: () async {
-            bool value = await Navigator.push(context, PageRouteBuilder(
-              opaque: false, pageBuilder: (context, _, __) => MyPopup(),
-              transitionsBuilder: (___, animation, ____, child) {
-               return FadeTransition(
-                   opacity: animation,
-                 child: ScaleTransition(scale: animation, child: child),
-               );
-            })
-            );
-
-            if (value) _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text("More"), backgroundColor: Colors.green));
-            else _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text('Less'), backgroundColor: Colors.red,));
-          }, child: Text("Find counter")),
-        ])
-      ),
-    );
-  }
-}
-
-class MyPopup extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-    return AlertDialog(
-      title: Text('Your answer'),
-      actions: <Widget>[
-        FlatButton(
-          onPressed: (){Navigator.pop(context, true);},
-          child: Text('more'),
-        ),
-        FlatButton(
-          onPressed: (){Navigator.pop(context, false);},
-          child: Text('less'),
-        )
-      ],
-    );
-  }
-}
 
 void main() => runApp(
 new MaterialApp(
-  home: MainScreen(),
+  home: Scaffold(body: MyApp()),
 )
 );
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State {
+  SandGlass clock = SandGlass();
+
+  @override
+  void initState () {
+    super.initState();
+
+    clock.tick();
+  }
+
+  _reDrawWidget()async{
+    if (clock.time() == 0) return;
+    await new Future.delayed(Duration(seconds: 1));
+    setState(() {
+    print('_reDrawWidget()');
+  });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _reDrawWidget();
+    return Center(child: Text("time is ${clock.time()}"));
+  }
+}
+
+class SandGlass {
+  int _sand = 0;
+
+  time(){
+    return _sand;
+  }
+
+  tick() async {
+    _sand = 100;
+    print('start tick');
+
+    while(_sand >0){
+      print('tick: $_sand');
+      _sand--;
+
+      await new Future.delayed(Duration(milliseconds: 100));
+    }
+    
+    print('end tick');
+  }
+}
